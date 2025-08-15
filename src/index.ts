31ca5e907a5e5f8e3e1d48d5830d69b14ec8ca67
“Isaac <isaac@isaacmason.com>
2025-08-15 11:41:29 +1000

    feat: initial version

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from "./generate";
+export * from "./query";
+export * as three from "./three";
---
+++ src/query/index.ts
@@ -0,0 +1,3 @@
+export type { NavMesh, NavMeshLink, NavMeshOffMeshConnection, NavMeshPoly, NavMeshPolyDetail, NavMeshTile, NodeRef } from "./nav-mesh";
+export * as navMesh from "./nav-mesh";
+export * as navMeshQuery from "./nav-mesh-query";
