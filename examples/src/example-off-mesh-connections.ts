9f3432b73fe331c95a9cf909083d62957be178e4
“Isaac <isaac@isaacmason.com>
2025-08-17 09:41:50 +1000

    feat: add off mesh connections example

---
+++ examples/src/example-off-mesh-connections.ts
@@ -0,0 +1,211 @@
+import type { Vec3 } from 'maaths';
+import {
+    addOffMeshConnection,
+    DEFAULT_QUERY_FILTER,
+    findPath,
+    getNodeRefType,
+    type NavMeshOffMeshConnection,
+    NodeType,
+    OffMeshConnectionDirection,
+    three as threeUtils,
+} from 'nav3d';
+import * as THREE from 'three';
+import { LineGeometry, OrbitControls } from 'three/examples/jsm/Addons.js';
+import { Line2 } from 'three/examples/jsm/lines/webgpu/Line2.js';
+import { Line2NodeMaterial } from 'three/webgpu';
+import { createExample } from './common/example-boilerplate';
+import {
+    generateTiledNavMesh,
+    type TiledNavMeshInput,
+    type TiledNavMeshOptions,
+} from './common/generate-tiled-nav-mesh';
+import { loadGLTF } from './common/load-gltf';
+
+/* setup example scene */
+const container = document.getElementById('root')!;
+const { scene, camera, renderer } = await createExample(container);
+
+camera.position.set(-2, 10, 10);
+
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+orbitControls.enableDamping = true;
+
+const navTestModel = await loadGLTF('/models/nav-test.glb');
+scene.add(navTestModel.scene);
+
+/* generate navmesh */
+const walkableMeshes: THREE.Mesh[] = [];
+scene.traverse((object) => {
+    if (object instanceof THREE.Mesh) {
+        walkableMeshes.push(object);
+    }
+});
+
+const [positions, indices] = threeUtils.getPositionsAndIndices(walkableMeshes);
+
+const navMeshInput: TiledNavMeshInput = {
+    positions,
+    indices,
+};
+
+const cellSize = 0.15;
+const cellHeight = 0.15;
+
+const tileSizeVoxels = 64;
+const tileSizeWorld = tileSizeVoxels * cellSize;
+
+const walkableRadiusWorld = 0.1;
+const walkableRadiusVoxels = Math.ceil(walkableRadiusWorld / cellSize);
+const walkableClimbWorld = 0.5;
+const walkableClimbVoxels = Math.ceil(walkableClimbWorld / cellHeight);
+const walkableHeightWorld = 0.25;
+const walkableHeightVoxels = Math.ceil(walkableHeightWorld / cellHeight);
+const walkableSlopeAngleDegrees = 45;
+
+const borderSize = 4;
+const minRegionArea = 8;
+const mergeRegionArea = 20;
+
+const maxSimplificationError = 1.3;
+const maxEdgeLength = 12;
+
+const maxVerticesPerPoly = 5;
+const detailSampleDistance = 6;
+const detailSampleMaxError = 1;
+
+const navMeshConfig: TiledNavMeshOptions = {
+    cellSize,
+    cellHeight,
+    tileSizeVoxels,
+    tileSizeWorld,
+    walkableRadiusWorld,
+    walkableRadiusVoxels,
+    walkableClimbWorld,
+    walkableClimbVoxels,
+    walkableHeightWorld,
+    walkableHeightVoxels,
+    walkableSlopeAngleDegrees,
+    borderSize,
+    minRegionArea,
+    mergeRegionArea,
+    maxSimplificationError,
+    maxEdgeLength,
+    maxVerticesPerPoly,
+    detailSampleDistance,
+    detailSampleMaxError,
+};
+
+const navMeshResult = generateTiledNavMesh(navMeshInput, navMeshConfig);
+const navMesh = navMeshResult.navMesh;
+
+/* add off mesh connections */
+const offMeshConnections: NavMeshOffMeshConnection[] = [
+    {
+        start: [-2.4799404316645157, 0.26716880587122915, 4.039628947351325],
+        end: [-2.735661224133032, 2.3264200687408447, 0.9084349415865054],
+        direction: OffMeshConnectionDirection.START_TO_END,
+        radius: 0.5,
+        cost: 0,
+        area: 0,
+        flags: 0xffffff,
+    },
+];
+
+for (const connection of offMeshConnections) {
+    addOffMeshConnection(navMesh, connection);
+}
+
+/* create debug helpers */
+const navMeshHelper = threeUtils.createNavMeshHelper(navMesh);
+navMeshHelper.object.position.y += 0.1;
+scene.add(navMeshHelper.object);
+
+const offMeshConnectionsHelper =
+    threeUtils.createNavMeshOffMeshConnectionsHelper(navMesh);
+scene.add(offMeshConnectionsHelper.object);
+
+/* find path */
+const start: Vec3 = [-3.94, 0.26, 4.71];
+const end: Vec3 = [2.52, 2.39, -2.2];
+const halfExtents: Vec3 = [1, 1, 1];
+
+const pathResult = findPath(
+    navMesh,
+    start,
+    end,
+    halfExtents,
+    DEFAULT_QUERY_FILTER,
+);
+
+console.log(pathResult);
+
+if (pathResult.success) {
+    const { path, nodePath } = pathResult;
+
+    if (nodePath) {
+        if (nodePath.intermediates?.nodes) {
+            const searchNodesHelper = threeUtils.createSearchNodesHelper(
+                nodePath.intermediates.nodes,
+            );
+            scene.add(searchNodesHelper.object);
+        }
+
+        for (let i = 0; i < nodePath.path.length; i++) {
+            const node = nodePath.path[i];
+
+            if (getNodeRefType(node) === NodeType.GROUND_POLY) {
+                const polyHelper = threeUtils.createNavMeshPolyHelper(
+                    navMesh,
+                    node,
+                );
+                polyHelper.object.position.y += 0.15;
+                scene.add(polyHelper.object);
+            }
+        }
+    }
+
+    if (path) {
+        for (let i = 0; i < path.length; i++) {
+            const point = path[i];
+
+            // point
+            const mesh = new THREE.Mesh(
+                new THREE.SphereGeometry(0.2),
+                new THREE.MeshBasicMaterial({ color: 0xff0000 }),
+            );
+            mesh.position.set(...point.position);
+            scene.add(mesh);
+
+            // line
+            if (i > 0) {
+                const prevPoint = path[i - 1];
+
+                const geometry = new LineGeometry();
+                geometry.setFromPoints([
+                    new THREE.Vector3(...prevPoint.position),
+                    new THREE.Vector3(...point.position),
+                ]);
+
+                const material = new Line2NodeMaterial({
+                    color: 'yellow',
+                    linewidth: 0.1,
+                    worldUnits: true,
+                });
+
+                const line = new Line2(geometry, material);
+
+                scene.add(line);
+            }
+        }
+    }
+}
+
+/* start loop */
+function update() {
+    requestAnimationFrame(update);
+
+    orbitControls.update();
+    renderer.render(scene, camera);
+}
+
+update();
