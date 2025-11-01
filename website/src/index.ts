0e99fa6509bc72dd91e814d9bb2bbf0a859438d3
“Isaac <isaac@isaacmason.com>
2025-11-01 10:26:19 +1000

    feat: website tweak

--- website/src/index.ts
+++ website/src/index.ts
@@ -46,7 +46,7 @@ const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
 scene.add(ambientLight);
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-directionalLight.position.set(5, 10, 5);
+directionalLight.position.set(-5, 10, 2);
 directionalLight.castShadow = true;
 directionalLight.shadow.mapSize.width = 2048;
 directionalLight.shadow.mapSize.height = 2048;
