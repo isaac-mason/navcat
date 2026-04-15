fb2f032e4152f76d08c083d2471aaf300f5fe8c2
“Isaac <isaac@isaacmason.com>
2026-04-15 12:33:39 +1000

    fix: polygonsToNavMeshTilePolys incorrect argument order for buildPolyNeighbours

--- src/generate/nav-mesh-tile.ts
+++ src/generate/nav-mesh-tile.ts
@@ -88,7 +88,7 @@ export const polygonsToNavMeshTilePolys = (
     const maxX = bounds[3];
     const maxZ = bounds[5];
 
-    buildPolyNeighbours(polys, vertices, borderSize, minX, maxX, minZ, maxZ);
+    buildPolyNeighbours(polys, vertices, borderSize, minX, minZ, maxX, maxZ);
 
     return {
         vertices,
@@ -154,10 +154,7 @@ export const polysToTileDetailMesh = (polys: NavMeshPoly[]): NavMeshTileDetailMe
  * @param polyMeshDetail
  * @returns
  */
-export const polyMeshDetailToTileDetailMesh = (
-    polys: NavMeshPoly[],
-    polyMeshDetail: PolyMeshDetail,
-) => {
+export const polyMeshDetailToTileDetailMesh = (polys: NavMeshPoly[], polyMeshDetail: PolyMeshDetail) => {
     const detailMeshes: NavMeshPolyDetail[] = [];
     const detailVertices: number[] = [];
 
